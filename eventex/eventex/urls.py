a0c034e0785f895fdf65affd5838a4fb6945abc0
alissonbtt <alison_btt@hotmail.com>
2021-09-08 12:46:23 -0300

    criando home page

--- eventex/eventex/urls.py
+++ eventex/eventex/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-
+from core.views import home
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
