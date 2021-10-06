1e6fab8066dc2e18edb926629913d3b85d316c00
alissonbtt <alison_btt@hotmail.com>
2021-10-06 14:59:52 -0300

    alterando index

--- eventex/eventex/urls.py
+++ eventex/eventex/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from core.views import home
+
+
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
