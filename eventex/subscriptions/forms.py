c73c93be73654575e201bd312514660549d71b4e
Alisson Bittencourt <alison_btt@hotmail.com>
2021-10-26 11:00:17 -0300

    criando modelform

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,15 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from subscriptions.validators import validate_cpf
+from subscriptions.models import Subscription
 
-
-def validate_cpf(value):
-    if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números', 'digits')
-
-    if len(value) != 11:
-        raise ValidationError('CPF deve ter 11 números', 'length')
-    
-class SubscriptionForm(forms.Form):
+   
+class SubscriptionFormOld(forms.Form):
     name = forms.CharField(label='Nome', max_length=150)
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
     email = forms.EmailField(label='Email', required=False)
@@ -26,3 +21,23 @@ class SubscriptionForm(forms.Form):
             raise ValidationError('Informe seu e-mail ou telefone.')
         
         return  self.cleaned_data
+    
+class SubscriptionForm(forms.ModelForm): 
+    
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email','phone']
+        
+    def clean_name(self):
+        name = self.cleaned_data['name'].title()
+        return name
+    
+    def clean(self):
+        self.cleaned_data = super().clean()
+        if not self.cleaned_data.get('email') and \
+        not self.cleaned_data.get('phone'):
+            
+            raise ValidationError('Informe seu e-mail ou telefone.')
+        
+        return  self.cleaned_data
+    
